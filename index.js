87312489b4624de6685a7e0161b8667e1d647c64
jeremaiiiahmata <matajoshuajeremaiah@gmail.com>
2024-09-16 23:01:07 +0800

    Add compose and submit functionalities

--- index.js
+++ index.js
@@ -5,11 +5,40 @@ import morgan from 'morgan';
 const app = express();
 const port = 3000; 
 
+let blogData = [];
+
+const logArray = (req, res, next) => {
+    console.table(blogData);
+    next();
+}
+
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(morgan("tiny"));
+app.use(logArray);
+
+app.post("/submit", (req, res) => {
+    const data = {
+        title : req.body['title'], 
+        author : req.body['author'],
+        content : req.body['content']
+    }
+
+    console.log(data);
+    blogData.push(data);
+    console.log("Data Added");
+
+    console.table(blogData);
+    res.redirect('/');
+})
+
+app.get("/compose", (req, res) => {
+    res.render("compose.ejs");
+})
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", { 
+        data : blogData 
+    });
 })
 
 app.listen(port, () => {
